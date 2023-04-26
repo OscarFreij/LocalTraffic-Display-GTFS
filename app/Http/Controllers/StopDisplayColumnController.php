1506e579afcacb283e47d7cb1a09088b5afecdc7
Oscar Freij <otg020313@gmail.com>
2023-04-26 22:52:41 +0200

    remove unused namespaces

--- app/Http/Controllers/StopDisplayColumnController.php
+++ app/Http/Controllers/StopDisplayColumnController.php
@@ -5,11 +5,9 @@ namespace App\Http\Controllers;
 use Exception;
 use App\Models\Stop;
 use App\Models\Stop_time;
-use PhpParser\Node\Stmt\TryCatch;
 use Illuminate\Support\Facades\DB;
 use App\Models\WorkerDataRetrieval;
 use DateTime;
-use Illuminate\Support\Facades\Log;
 use Illuminate\Database\Query\Builder;
 
 class StopDisplayColumnController extends Controller
