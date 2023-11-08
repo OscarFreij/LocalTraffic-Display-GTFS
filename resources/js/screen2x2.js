fb65772527fb02bdf2cedd7e049bb38d623e3f97
Oscar Freij <otg020313@gmail.com>
2023-11-08 22:41:35 +0100

    fix long and complex load of 2x2 screens

--- resources/js/screen2x2.js
+++ resources/js/screen2x2.js
@@ -1,40 +1,21 @@
 //var stopQue = ["9021001005031000","9021001050392000","9021001050012000"];
 
-var newData = ""
-var firstRun = true;
-
-const delay = ms => new Promise(res => setTimeout(res, ms));
-
-async function LoadNewColumns()
+function LoadNewColumns()
 {
-    if (!firstRun)
-    {
-        document.getElementById('data').innerHTML = newData;
-        newData = ""
-    }
-    else
-    {
-        firstRun = false;
-    }
-    
-    
+    var newData = ""
     for (let i = 0; i < stopQue.length; i++) {
-        var readyToContinue = false
         var stop = stopQue[i];
         var xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             newData += this.responseText;
-            readyToContinue = true;
         }
         };
-        xhttp.open("GET", "https://localtraffic.retune365.com/displayAPI/"+stop+"/2x2/4", true);
+        xhttp.open("GET", "https://localtraffic.retune365.com/displayAPI/"+stop+"/2x2/5", false);
         xhttp.send();
-        while (!readyToContinue)
-        {
-            await delay(1000);
-        }
     }
+
+    document.getElementById('data').innerHTML = newData;
 }
 
 
--- resources/views/screen2x2.blade.php
+++ resources/views/screen2x2.blade.php
@@ -13,7 +13,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
   <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="viewport" content="width=device-width, initial-scale=0.5">
   <script>var stopQue = {{ Js::from($carousel_data_array) }};</script>
   @vite('resources/css/app.css')
   @vite('resources/css/statusBox.css')
